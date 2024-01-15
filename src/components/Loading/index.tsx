c497b79bf7eace2d1b20b43b83c6832d28150852
Douglas Domiciano <douglasdsdomiciano@hotmail.com>
2024-01-15 17:03:18 -0300

    Loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,30 @@
-import { View, ActivityIndicator } from "react-native";
 import React from "react";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
 
-const Laoding = () => {
+interface LoadingComponentProps {
+  message?: string;
+}
+
+const Laoding: React.FC<LoadingComponentProps> = ({ message }) => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <ActivityIndicator color={"#0A3750"} size={42} />
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#0A3750" />
+      {message && <Text style={styles.message}>{message}</Text>}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  message: {
+    marginTop: 10,
+    fontSize: 16,
+    color: "#333",
+  },
+});
 
 export default Laoding;
